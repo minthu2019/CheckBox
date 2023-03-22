cff3609fece7928866a1a9500dd15e170ecb2d63
min thu khaing <mtk.min2003@gmail.com>
2023-03-22 23:29:37 +0630

    JSX Rule

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";            //call the react libaray because we will use the react app
+import ReactDOM from "react-dom";             // call the react-dom because we want to see the react componant on screen
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// function Greeting () {                                                         //react conponant
+//   return (<div>
+//             <h1> Hello react , My name is mis thu khaing,thanks  learnign me</h1>
+//             <p> Min thu khaing</p>
+//           </div>) ;       //JSX
+// }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// alway return single element
+// div / section / article ot Fragment
+// use camelCase for html attribute
+// use class name instead of class
+// close every elemnt
+// formatting
+
+// we call the html as JSX in react;
+
+function Greeting() {
+  return React.createElement( "div" , {} , React.createElement("h1" , {} , " Min Thu Khaing")) //same the upper function
+}
+
+ReactDOM.render(<Greeting/> , document.getElementById("root") );     //  render = show the screen
+
